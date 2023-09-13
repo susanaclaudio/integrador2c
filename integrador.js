1c7e7a674438e825a3a714a0e17b4ede80f13335
susanaclaudio <135056622+susanaclaudio@users.noreply.github.com>
2023-09-13 12:13:11 -0500

    Add files via upload

---
+++ integrador.js
@@ -0,0 +1,11 @@
+alert("Bienvenidos a la polleria Corralito")
+entrada=parseInt(prompt("ingrese el valor de la entrada"))
+segundo=parseInt(prompt("ingrese el valor de la entrada"))
+postre=parseInt(prompt("ingrese el valor de la entrada"))
+total=entrada+segundo+postre
+igv=total*18/100
+console.log("El valor de la entrada es:",entrada)
+console.log("El valor del segundo es:",segundo)
+console.log("El valor del postre es:",postre)
+console.log("El valor total es:",total)
+console.log("El costo total con igv es:",igv+total)
